c6799cfe3beb1eeba203ba9ce77d591203975ee2
1056927138@qq.com <shim@fusen.net.cn>
2020-09-08 12:15:49 +0800

    first commit

---
+++ src/main/java/com/shiming/redisproj/RedisprojApplication.java
@@ -0,0 +1,13 @@
+package com.shiming.redisproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisprojApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisprojApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shiming/redisproj/RedisprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiming.redisproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisprojApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
