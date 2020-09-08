8ebc9b5569b7289859abd2e198959ebe6419fe83
1056927138@qq.com <shim@fusen.net.cn>
2020-09-08 21:31:34 +0800

    提交redis配置

--- src/test/java/com/shiming/redisproj/RedisprojApplicationTests.java
+++ src/test/java/com/shiming/redisproj/RedisprojApplicationTests.java
@@ -1,13 +1,24 @@
 package com.shiming.redisproj;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.Redisson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class RedisprojApplicationTests {
 
+    @Autowired
+    Redisson redisson;
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+
+        System.out.println("23e23e23");
     }
 
 }
