764b40296cb87ab80aa5b8c50afd0f7f00a8d715
1056927138@qq.com <shim@fusen.net.cn>
2020-09-28 11:55:48 +0800

    添加数据源配置以及测试

--- src/main/java/com/shiming/redisproj/RedisprojApplication.java
+++ src/main/java/com/shiming/redisproj/RedisprojApplication.java
@@ -3,10 +3,9 @@ package com.shiming.redisproj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration
 public class RedisprojApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/shiming/redisproj/RedisprojApplicationTests.java
+++ src/test/java/com/shiming/redisproj/RedisprojApplicationTests.java
@@ -5,6 +5,10 @@ import org.redisson.Redisson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class RedisprojApplicationTests {
@@ -15,10 +19,21 @@ class RedisprojApplicationTests {
     @Autowired
     StringRedisTemplate redisTemplate;
 
+    @Autowired
+    JdbcTemplate mysqlJdbcTemplate;
+
     @Test
     void contextLoads() {
 
-        System.out.println("23e23e23");
+        String name=redisTemplate.opsForValue().get("name");
+        System.out.println(name);
     }
 
+    @Test
+    void contextLoads2() {
+
+        List<Map<String,Object>> res=mysqlJdbcTemplate.queryForList("select * from user");
+
+        System.out.println("233232");
+    }
 }
