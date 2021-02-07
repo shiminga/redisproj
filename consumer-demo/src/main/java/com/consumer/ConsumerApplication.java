dc6a27331eefea403781466143033be1e218857a
1056927138@qq.com <shim@fusen.net.cn>
2021-02-07 17:49:19 +0800

    dubbo构建工程提交

---
+++ consumer-demo/src/main/java/com/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+
+    }
+
+}
--- shiming-producer-demo/src/main/java/com/shiming/redisproj/service/SaleService.java
+++
@@ -1,8 +0,0 @@
-package com.shiming.redisproj.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface SaleService {
-    int sale(int saleNum);
-}
