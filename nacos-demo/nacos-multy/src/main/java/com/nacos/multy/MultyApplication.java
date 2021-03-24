be7fe0b90382c8c3265702251586245e46911d0a
1056927138@qq.com <1056927138@qq.com>
2021-03-24 17:33:56 +0800

    提交nacos代码

---
+++ nacos-demo/nacos-multy/src/main/java/com/nacos/multy/MultyApplication.java
@@ -0,0 +1,13 @@
+package com.nacos.multy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultyApplication.class, args);
+    }
+
+}
---
+++ nacos-demo/nacos-multy/src/test/java/com/nacos/multy/MultyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacos.multy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
