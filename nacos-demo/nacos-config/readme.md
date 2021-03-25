03f5d0a569ead0b5859d5290fd05c5b83450fa96
1056927138@qq.com <1056927138@qq.com>
2021-03-25 11:54:32 +0800

    提交nacos配置中心的代码4

---
+++ nacos-demo/nacos-config/readme.md
@@ -0,0 +1,20 @@
+通过 Nacos 的 @NacosValue 注解设置属性值。
+@Controller
+@RequestMapping("config")
+public class ConfigController {
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    @RequestMapping(value = "/get", method = GET)
+    @ResponseBody
+    public boolean get() {
+        return useLocalCache;
+    }
+}
+启动 NacosConfigApplication，调用 curl http://localhost:8080/config/get，返回内容是 false。
+
+通过调用 Nacos Open API 向 Nacos server 发布配置：dataId 为example，内容为useLocalCache=true
+
+curl -X POST "http://127.0.0.1:8848/nacos/v1/cs/configs?dataId=example&group=DEFAULT_GROUP&content=useLocalCache=true"
+再次访问 http://localhost:8080/config/get，此时返回内容为true，说明程序中的useLocalCache值已经被动态更新了。
---
+++ nacos-demo/nacos-config/src/main/java/com/nacos/config/controller/ConfigController.java
@@ -0,0 +1,27 @@
+package com.nacos.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value(value = "${useLocalCache:false}")
+    private boolean useLocalCache;
+
+
+    @Value(value = "${myName:ming}")
+    private String myName;
+    /**
+     * http://localhost:8080/config/get
+     */
+    @RequestMapping("/get")
+    public String get() {
+        System.out.println(Thread.currentThread().getName()+":"+this);
+        return useLocalCache+","+myName;
+    }
+}
