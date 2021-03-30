4a7fc7890ce59f41530c7a0bc895f075cc70f60a
1056927138@qq.com <1056927138@qq.com>
2021-03-30 19:39:47 +0800

    提交3.30号下午刷题

--- producer-demo/src/main/java/com/producer/practice/QS.java
+++ producer-demo/src/main/java/com/producer/practice/QS.java
@@ -25,13 +25,16 @@ public class QS {
 
 //        spiralOrder();
 
-        int[][] arr=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
-        System.out.println(arr instanceof int[][]);
-
-        System.out.println(oneEditAway("baaa","b"));
-        spiralOrder(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
-//        longestSubarray(new int[]{88,10},10);
-        reverseBetween(new ListNode(3,new ListNode(5,null)),2,4);
+//        int[][] arr=new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+//        System.out.println(arr instanceof int[][]);
+//
+//        System.out.println(oneEditAway("baaa","b"));
+//        spiralOrder(new int[][]{{1,2,3},{4,5,6},{7,8,9}});
+////        longestSubarray(new int[]{88,10},10);
+//        reverseBetween(new ListNode(3,new ListNode(5,null)),2,4);
+int[][] nums={{0,1,0,0,1,1,0},{1,0,0,0,0,0,0},{1,0,0,1,1,1,1},{0,1,0,0,0,0,0},{1,0,0,0,0,0,1},{1,0,0,1,0,0,0},{1,0,1,0,0,1,0}};
+nums=new int[][]{{0,1,0},{1,1,0},{0,0,0}};
+        System.out.println(shortestPathBinaryMatrix2(nums));
     }
 
     public static void quickSort(int[] nums, int l,int r){
@@ -496,4 +499,161 @@ public class QS {
         }
         return returnHead;
     }
+
+    /**
+     * 给你一个 n x n 的二进制矩阵 grid 中，返回矩阵中最短 畅通路径 的长度。如果不存在这样的路径，返回 -1 。
+     *
+     * 二进制矩阵中的 畅通路径 是一条从 左上角 单元格（即，(0, 0)）到 右下角 单元格（即，(n - 1, n - 1)）的路径，该路径同时满足下述要求：
+     *
+     * 路径途经的所有单元格都的值都是 0 。
+     * 路径中所有相邻的单元格应当在 8 个方向之一 上连通（即，相邻两单元之间彼此不同且共享一条边或者一个角）。
+     * 畅通路径的长度 是该路径途经的单元格总数。
+     *
+     *
+     *
+     * 示例 1：
+     *
+     *
+     * 输入：grid = [[0,1],[1,0]]
+     * 输出：2
+     * 示例 2：
+     *
+     *
+     * 输入：grid = [[0,0,0],[1,1,0],[1,1,0]]
+     * 输出：4
+     * 示例 3：
+     *
+     * 输入：grid = [[1,0,0],[1,1,0],[1,1,0]]
+     * 输出：-1
+     *
+     *
+     * 提示：
+     *
+     * n == grid.length
+     * n == grid[i].length
+     * 1 <= n <= 100
+     * grid[i][j] 为 0 或 1
+     */
+    static int res=Integer.MAX_VALUE;
+    static boolean[][] used;
+
+    static int[][] rc={{0,1},{1,-1},{1,0},{1,1},{-1,-1},{-1,0},{-1,1},{0,-1}};
+    public static int shortestPathBinaryMatrix(int[][] grid) {
+//        if(grid==null||grid.length==0||grid[0][0]!=0){return -1;}
+//        int row=grid.length,col=grid[0].length;
+//        used=new boolean[row][col];
+//
+//        used[0][0]=true;
+//        bfs(grid,0,0,col,row,1);
+//        return res==Integer.MAX_VALUE?-1:res;
+        int row=grid.length,col=grid[0].length;
+
+        if(grid[0][0]==1||grid[row-1][col-1]==1){return -1;}
+
+        LinkedList<int[]> queue=new LinkedList<>();
+        int ans=0;
+        queue.add(new int[]{0,0});
+        ans++;
+        while(!queue.isEmpty()){
+            int size=queue.size();
+            while(size-->0){
+                int[] loc=queue.removeFirst();
+
+                for(int[] dirs:rc){
+                    int newX=loc[0]+dirs[0];
+                    int newY=loc[1]+dirs[1];
+
+                    if(newX==row-1&&newY==col-1){
+                        return ans+1;
+                    }
+
+                    if(newX>=0 && newX < col && newY >= 0 && newY < row&&grid[newX][newY] == 0){
+                        queue.add(new int[]{newX,newY});
+                        grid[newX][newY]=1;
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+
+    static void bfs(int[][] grid,int x,int y,int X,int Y,int len){
+        if(x==X-1&&y==Y-1){
+            res=Math.min(res,len);
+            System.out.println(res);
+            return;
+        }
+
+        for (int[] i : rc) {
+            int xi=x+i[0];
+            int yj=y+i[1];
+            if (xi>=0 && xi < X && yj >= 0 && yj < Y && !used[xi][yj] && grid[xi][yj] == 0) {
+                used[xi][yj] = true;
+                bfs(grid, xi, yj, X, Y, len + 1);
+                used[xi][yj] = false;
+            }
+        }
+    }
+
+    private static int[][] directions = {{0,1}, {0, -1}, {1, -1}, {1, 0}, {1, 1}, {-1, -1}, {-1, 0}, {-1, 1}};
+    private static int row, col;
+    public static int shortestPathBinaryMatrix2(int[][] grid) {
+        row = grid.length;
+        col = grid[0].length;
+        if(grid[0][0] == 1 || grid[row - 1][col - 1] == 1) return -1;
+        Queue<int[]> pos = new LinkedList<>();
+        grid[0][0] = 1; // 直接用grid[i][j]记录从起点到这个点的最短路径长。按照题意 起点也有长度1
+        pos.add(new int[]{0,0});
+        while(!pos.isEmpty() && grid[row - 1][col - 1] == 0){ // 求最短路径 使用BFS
+            int[] xy = pos.remove();
+            int preLength = grid[xy[0]][xy[1]]; // 当前点的路径长度
+            for(int i = 0; i < 8; i++){
+                int newX = xy[0] + directions[i][0];
+                int newY = xy[1] + directions[i][1];
+                if(inGrid(newX, newY) && grid[newX][newY] == 0){
+                    pos.add(new int[]{newX, newY});
+                    grid[newX][newY] = preLength + 1; // 下一个点的路径长度要+1
+                    System.out.println(pos.size());
+                }
+            }
+        }
+        List<int[]> queue=new LinkedList<>();
+        return grid[row - 1][col - 1] == 0 ? -1 : grid[row - 1][col - 1]; // 如果最后终点的值还是0，说明没有到达
+    }
+
+    private static boolean inGrid(int x, int y){
+        return x >= 0 && x < row && y >= 0 && y < col;
+    }
+
+//    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+//
+//        int M=grid.size(),N=grid[0].size();
+//        if(grid[0][0]==1||grid[M-1][N-1]==1)return -1;
+//        queue<vector<int>>que;
+//        que.push({0,0});
+//        vector<vector<int>>dir={{0,1},{0,-1},{1,0},{-1,0},{1,1},{1,-1},{-1,1},{-1,-1}};
+//        int res=1;
+//        while(!que.empty()){
+//            int size=que.size();
+//            for(int i=0;i<size;++i){
+//                auto cur=que.front();
+//                int x=cur[0];
+//                int y=cur[1];
+//                que.pop();
+//                if(x==M-1&&y==N-1)return res;
+//                for(vector<int>& d:dir){
+//                    int nx=x+d[0];
+//                    int ny=y+d[1];
+//                    if(nx<0||nx>=M||ny<0||ny>=N||grid[nx][ny]==1)continue;
+//
+//                    grid[nx][ny]=1;
+//                    que.push({nx,ny});
+//                }
+//            }
+//            res++;
+//        }
+//        return -1;
+//
+//    }
 }
